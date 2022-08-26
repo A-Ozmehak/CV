eece9cf528122091251f88f63d68362c3921e705
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-08-26 02:01:03 +0200

    added placeholder images for the frontpage

---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+    return (
+        <header>
+            <h1>Välkommen</h1>
+        </header>
+    )
+}
+
---
+++ src/pages/landingpage.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/header";
+import {StartPageImages} from "../components/startPageImages";
+
+export const LandingPage = () => {
+    return (
+        <div>
+        <Header />
+        <StartPageImages />
+        </div>
+    )
+}
