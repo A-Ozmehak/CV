da4e2f517fe02adb73c990864c852acdae1cc5fc
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-08-28 06:50:38 +0200

    added component files for the cv pages, flag icons for different languages

---
+++ src/components/landingPageImages.jsx
@@ -0,0 +1,41 @@
+import dog4 from "../assets/dog4.jpg"
+import dog2 from "../assets/dog2.jpg"
+import "../css/landingpage.css"
+import {useState} from "react";
+import { Link } from "react-router-dom";
+
+export const LandingPageImages = () => {
+    const [isShown, setIsShown] = useState(false)
+    const [showSecond, setShowSecond] = useState(false)
+
+    return (
+        <div>
+            <div>
+                <Link to={"/calleOzmehak"}>
+                <button
+                    onMouseEnter={() => setIsShown(true)}
+                    onMouseLeave={() => setIsShown(false)}
+                >
+                    <img className="dogImage" src={dog4} alt="dog1"/>
+                </button>
+                </Link>
+                {isShown && (
+                    <h1>Calle</h1>
+                )}
+            </div>
+            <div>
+                <Link to={"/annaOzmehak"}>
+                <button
+                    onMouseEnter={() => setShowSecond(true)}
+                    onMouseLeave={() => setShowSecond(false)}
+                >
+                    <img className="dogImage" src={dog2} alt="dog2"/>
+                </button>
+                </Link>
+                {showSecond && (
+                    <h1>Anna</h1>
+                )}
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/mrsOzmehak.jsx
@@ -0,0 +1,20 @@
+import {FlagIcons} from "../components/flagIcons";
+import {AnnaInfoSwe} from "../components/swe/annaInfoSwe";
+import {AnnaInfoEng} from "../components/eng/annaInfoEng";
+import {AnnaInfoSka} from "../components/ska/annaInfoSka";
+import {useState} from "react";
+
+export const MrsOzmehak = () => {
+    // const [showSwe, setShowSwe] = useState(true)
+    // const [showEng, setShowEng] = useState(false)
+    // const [showSka, setShowSka] = useState(false)
+    return(
+        <div>
+            <header>
+                <h1>Anna's CV</h1>
+                <FlagIcons />
+            </header>
+
+            </div>
+    )
+}
